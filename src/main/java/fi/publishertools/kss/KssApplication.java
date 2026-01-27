d78369a0946152a3cb45f50c4da0eb55237dec6a
kimmohakkarainen <30873934+kimmohakkarainen@users.noreply.github.com>
2026-01-27 11:21:01 +0200

    Add files via upload

---
+++ src/main/java/fi/publishertools/kss/KssApplication.java
@@ -0,0 +1,13 @@
+package fi.publishertools.kss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KssApplication.class, args);
+	}
+
+}
