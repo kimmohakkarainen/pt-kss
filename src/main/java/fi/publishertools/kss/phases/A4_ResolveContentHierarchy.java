f01ced7466b5e6bfecb09228bb6965db468c7638
Kimmo Hakkarainen <kimmo.hakkarainen@atoz.fi>
2026-02-08 20:23:47 +0200

    collect hierarchy information - just print those into log for analysis

--- src/main/java/fi/publishertools/kss/phases/A4_ResolveContentHierarchy.java
+++ src/main/java/fi/publishertools/kss/phases/A4_ResolveContentHierarchy.java
@@ -2,7 +2,11 @@ package fi.publishertools.kss.phases;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
+import java.util.Stack;
+import java.util.TreeMap;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -35,6 +39,11 @@ public class A4_ResolveContentHierarchy extends ProcessingPhase {
 		}
 
 		List<ChapterNode> reorganized = simplifyStyles(chapters);
+
+		StyleTable table = collectStyles(reorganized, new StyleTable());
+
+		logger.info(table.toString());
+
 
 		context.setChapters(reorganized);
 		logger.debug("Resolved content hierarchy for file {}", context.getFileId());
@@ -70,5 +79,97 @@ public class A4_ResolveContentHierarchy extends ProcessingPhase {
 	private String simplifyStyle(String style) {
 		String [] splitted = style == null ? null : style.split("/");
 		return splitted == null ? null : splitted[splitted.length - 1];
+	}
+
+	private StyleTable collectStyles(List<ChapterNode> input, StyleTable output) {
+
+		for(ChapterNode node : input) {
+			if(node.appliedStyle() != null) {
+				output.add(node.appliedStyle());
+			}
+			if(node instanceof StoryNode) {
+				output.push();
+				output = collectStyles(node.children(), output);
+				output.pop();
+			} else if(node instanceof ParagraphStyleRangeNode) {
+				output.push();
+				output = collectStyles(node.children(), output);
+				output.pop();
+			}  
+		}
+		return output;
+	}
+
+	public static class StyleTable {
+
+		private Map<String, Map<String, Long>> table = new TreeMap<>();
+		private String previous = "";
+		private Stack<String> stack = new Stack<>();
+		
+		
+		public void add(String next) {
+			put(this.previous, next);
+			this.previous = next;
+		}
+
+		public void addAll(Collection<String> col) {
+			for(String next : col) {
+				this.add(next);
+			}
+		}
+		
+		public void push() {
+			stack.push(this.previous);
+			this.previous = "";
+		}
+		
+		public void pop() {
+			this.previous = stack.pop();
+		}
+		
+
+		private void put(String previous, String next) {
+			checkExistence(previous, next);
+			Map<String, Long> row = table.get(previous);
+			row.put(next,  1L + row.get(next));
+		}
+
+		private void checkExistence(String previous, String next) {
+			checkExistence(previous);
+			checkExistence(next);
+		}
+
+		private void checkExistence(String word) {
+			Map<String, Long> row = table.get(word);
+			if(row == null) {
+				row = new TreeMap<>();
+				table.put(word, row);
+				for(String key : table.keySet()) {
+					row.put(key, 0L);
+					table.get(key).put(word, 0L);
+				}
+			}
+		}
+
+		@Override
+		public String toString() {
+			StringBuilder builder = new StringBuilder();
+			builder.append("\n,");
+			for(String col: table.keySet()) {
+				builder.append(col);
+				builder.append(",");
+			}
+			builder.append("\n");
+			for(String row: table.keySet()) {
+				builder.append(row);
+				builder.append(",");
+				for(String col: table.keySet()) {
+					builder.append(table.get(row).get(col));
+					builder.append(",");
+				}
+				builder.append("\n");
+			}
+			return builder.toString();
+		}
 	}
 }
